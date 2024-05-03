a35bd09fbf31cbcdf3d2500aa154411cc6e7d432
DzaboffCodes <142997851+DzaboffCodes@users.noreply.github.com>
2024-05-03 13:07:31 -0500

    Making the Nav-Bar Responsive

---
+++ main.js
@@ -0,0 +1,22 @@
+// Navigation Bar
+function myMenuFunction() {
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+// Add Shadow on Navigation Bar While Scrolling
+
+
+// Typing Effect
+
+
+// Scroll Reveal Animation
+
+// Home
+
+// Projet Box 
